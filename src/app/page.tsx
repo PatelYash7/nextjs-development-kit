cac7f11bfd2f8bc5d7c88201b4d20e98ff122e90
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2025-01-03 00:03:46 +0530

    Updated the copy to clipboard.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Clipboard } from '@/components/clipboard';
 import { AuroraBackground } from '@/components/ui/aurora-background';
 import { FloatingNav } from '@/components/ui/floating-navbar';
 import { IconHome, IconMessage, IconUser } from '@tabler/icons-react';
@@ -22,7 +23,9 @@ export default async function Home() {
 				<div className=' text-xl sm:text-3xl md:text-5xl font-bold dark:text-white text-center'>
 					Effortless NextJS Starter Kit for Developers
 				</div>
-				<div className='py-4 text-lg text-center px-6  sm:text-xl text-gray-400 dark:text-gray-400'>In cmd type <br /> <span className='text-white font-bold'> npx next-development-kit </span> <br /> to get started</div>
+				<div className='py-4'>
+					<Clipboard />
+				</div>
 				<p className=' sm:text-xl md:text-xl px-12 sm:px-24 py-8 font-semibold dark:text-gray-400 text-gray-400  text-center'>
 					This template is designed for modern developers who want to focus on
 					business logic while minimizing setup time.
